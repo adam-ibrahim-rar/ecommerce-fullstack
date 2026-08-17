c9a2823da6425213e263549789a9776b183d9d60
Adam Ibrahim <adamdomadoma@gmail.com>
2026-08-18 01:38:07 +0300

    feat: migrate from MongoDB to PostgreSQL with Prisma integration
    - Added Prisma dependencies to package.json for PostgreSQL support.
    - Removed MongoDB connection logic and related dependencies.
    - Updated environment configuration to include DATABASE_URL for PostgreSQL.
    - Created Prisma configuration and schema files for database setup.
    - Implemented Prisma client in the application for database interactions.
    - Updated TypeScript configuration for improved module resolution and compatibility.
    - Added .gitignore to exclude node_modules and environment variables.

--- Server/src/config/env.ts
+++ Server/src/config/env.ts
@@ -6,5 +6,6 @@ dotenv.config();
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI!,
   JWT_SECRET: process.env.JWT_SECRET!,
+  DATABASE_URL:process.env.DATABASE_URL
 };
 export default env;
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import env from "./config/env";
-import connectDB from "./config/db";
+import prisma from "./config/prisma";
+
 const app = express();
 const port = env.PORT;
-connectDB();
+
 
 
 app.listen(port, () => {
